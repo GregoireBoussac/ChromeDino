d1b08e3231f23fc8b8fc01df06814bbce3834563
GregoireBoussac <gregoire.boussac@ponts.org>
2018-04-11 12:00:43 +0200

    Add Canny edges and correct error
    - Correct error method Template Matching (error only in previous commit)
    - Add Canny Edges

--- dino.py
+++ dino.py
@@ -19,12 +19,11 @@ class Game():
     def __init__(self, selenium=True):
         self.selenium = selenium
         driver_path = '/Users/gregoire/Documents/git/ChromeDino/chromedriver'
-        if self.selenium:
-            chrome_options = Options()
-            #chrome_options.add_argument('--start-fullscreen')
-            self.driver = webdriver.Chrome(driver_path, options=chrome_options)
-            self.driver.get('https://chromedino.com/')
-            self.body = self.driver.find_element_by_css_selector('body')
+        chrome_options = Options()
+        #chrome_options.add_argument('--start-fullscreen')
+        self.driver = webdriver.Chrome(driver_path, options=chrome_options)
+        self.driver.get('https://chromedino.com/')
+        self.body = self.driver.find_element_by_css_selector('body')
         
         # Starts as soon as the page is fully loaded
         
@@ -32,38 +31,31 @@ class Game():
         self.h_mask = 130
         self.w_mask = 600
         
-        self.vision = Vision(self.h_mask, self.w_mask)
-        
-        self.game_over=False
-        
-        t = time.time()
+        # Launch recording and Vision system
+        self.vision = Vision(self.h_mask, self.w_mask)        
         self.vision.get_position_roi()
-        print('Get position ROI: ', time.time()-t)
-        self.roi = self.vision.grab_roi() # Get screenshot of initial state
         
-        cv2.imshow('Game', self.roi)
-        
+        # Launch game        
+        self.game_over=False
         self.start_game()
         self.play()
     
     def jump(self, action='up'):
-        if action is not None:
-            if self.selenium:
-                if action == 'up':
-                    self.body.send_keys(Keys.ARROW_UP)
-                    print('up')
-                else:
-                    self.body.send_keys(Keys.ARROW_DOWN)
-                    print('down')
-            else:
-                pyautogui.press(action)
-                print('Not selenium', action)
-                
+        if action == 'up':
+            self.body.send_keys(Keys.ARROW_UP)
+            print('up')
+        else:
+            self.body.send_keys(Keys.ARROW_DOWN)
+            print('down')
+          
+        
     def start_game(self):
         print('>> Start')
-        self.jump()
+        self.jump() # Jump to begin playing
         time.sleep(1)
-    
+        self.time_start = time.time() # Needs to be after time.sleep(1)
+
+        
     def play(self):
         while not self.game_over:
             self.get_action()
@@ -71,12 +63,15 @@ class Game():
             if self.is_game_over():
                 self.game_over = True
                 break
+        self.end_game()        
                 
-        print(">> Game Over!")
+    def end_game(self):
+        self.final_score = self.get_score()
+        print('>> Game Over!')
+        print('Final score: ', self.final_score)
         self.driver.quit()
         cv2.destroyAllWindows()
-
-
+    
     
     def get_action(self):
         self.roi = self.vision.grab_roi()
@@ -86,13 +81,18 @@ class Game():
         if p>0.5:
             self.jump('up')
     
+    
     def is_game_over(self):
         screen1 = self.vision.grab_roi()
         time.sleep(0.1)
         screen2 = self.vision.grab_roi()
         
         return np.array_equal(screen1, screen2)
-
+    
+    
+    def get_score(self):
+        return int((time.time()-self.time_start)*10)
+        
         
         
 class Vision():
@@ -105,13 +105,14 @@ class Vision():
         cv2.namedWindow(self.window_name,cv2.WINDOW_AUTOSIZE)
         cv2.moveWindow(self.window_name, 0, 0)
         
+        
     def get_position_roi(self):
         dino = np.asarray(Image.open(r"images/dino.png"))
         screenshot  = np.asarray(ImageGrab.grab())
         
         img = screenshot.copy()
         template = dino.copy()
-        method = cv2.TM_CCORR
+        method = cv2.TM_CCOEFF
 
         w, h, _ = template.shape[::-1]
         
@@ -122,16 +123,22 @@ class Vision():
         top_left = max_loc
         self.bottom_left_roi = (top_left[0], top_left[1] + h)
     
+    
     def grab_roi(self):
         bottom_left_roi_x, bottom_left_roi_y = self.bottom_left_roi
         
         roi = tools.screen_capture(bottom_left_roi_y - self.h_mask, bottom_left_roi_x, self.w_mask, self.h_mask)
         
         # Displays what the algorithm sees
-        cv2.imshow(self.window_name, roi)
-        cv2.waitKey(1)
+        self.display_image(roi)
         
         return roi
+    
+    def display_image(self, roi):
+        edges = cv2.Canny(roi, 100, 200)
+
+        cv2.imshow(self.window_name, edges)
+        cv2.waitKey(1)
     
         
 if __name__ == '__main__':
